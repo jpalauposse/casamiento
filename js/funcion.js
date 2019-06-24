0246eb867a0e7221426fd81b4f5b11b63ebdea54
Javier Palau <javopa@yahoo.com.ar>
2019-06-24 20:46:41 -0300

    avanzamos con el desarrollo

--- js/funcion.js
+++ js/funcion.js
@@ -1,3 +1,4 @@
+
 function createList() {
   var conteiner = document.getElementById("marketing");
   var row = document.createElement('div');
@@ -5,15 +6,28 @@ function createList() {
     var col = document.createElement('div')
     col.innerHTML =
       `
-    <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="fotos/${regalos[i].foto}" alt="${regalos[i].alt}">
+    <img class="bd-placeholder-img rounded-circle foto" width="140" height="140" src="fotos/${regalos[i].foto}" alt="${regalos[i].alt}">
     <h2>${regalos[i].titulo}</h2>
-    <p>${regalos[i].precio}</p>
-    <p><a class="btn btn-secondary boton" href="#" role="button">Comprar</a></p>
+    <p>$ ${regalos[i].precio}</p>
+    <button class="btn btn-secondary boton" href="" role="button" onclick="seleccion()">Comprar</button>
+    <p></p>
     `;
-    col.className = "col-lg-3";
+    col.className = "col-lg-3 text-center";
     row.appendChild(col);
   }
   conteiner.appendChild(row);
-  row.className = "row"
+  row.className = "row";
+
 };
 createList();
+
+function seleccion(){
+var carrito = document.getElementById("fab");
+var obj = document.createElement("a");
+obj.innerHTML = `
+<i class="fas fa-cart-arrow-down"></i>
+`;
+obj.href = "pages/carrito.html";
+carrito.appendChild(obj);
+
+};
