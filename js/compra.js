c1ae6e2b40cbb264e850736bc17c4b742d17ab4c
Javier Palau <javopa@yahoo.com.ar>
2019-07-03 01:25:55 -0300

    Se corrijen las funciones de compra.

--- js/compra.js
+++ js/compra.js
@@ -1,22 +1,52 @@
+comprado = localStorage.getItem("comprado");
+comprado = JSON.parse(comprado);
 
-function comprado(){
+function compra(){
+  var cont = 0;
   var tabla = document.getElementById("personajes");
-  for(var i = 0; i < regalos.length; i++){
+  for(var i = 0; i < comprado.length; i++){
       var tr = document.createElement('tr');
       tr.innerHTML = `
-        <td>${regalos[i].titulo}</td>
-        <td>${regalos[i].precio}</td>
-        <td><button type="button" class="btn btn-danger">Quitar</button> </td>
+        <td>${comprado[i].description}</td>
+        <td>${comprado[i].monto}</td>
+        <td><button type="button" class="btn btn-danger" onclick="borrar(${comprado[i].monto})">Quitar</button> </td>
       `;
-      tr.id = regalos[i].precio;
+      tr.id = comprado[i].monto;
       tabla.appendChild(tr);
+      var sum = parseInt(comprado[i].monto);
+      cont = cont + sum;
     }
     var td = document.createElement('tr');
     td.innerHTML = `
     <td>Total</td>
-    <td></td>
+    <td id="total">$${cont}</td>
     <td><button type="button" class="btn btn-danger">Quitar todos</button> </td>
     `;
     tabla.appendChild(td)
   };
-  comprado();
+  compra();
+
+function deletep(ubicacion, tabla){
+tabla.splice(ubicacion, 1)
+};
+function eliminarSeleccion(monto, matriz){
+  for(i = 0; i < matriz.length; i++){
+    var rev = matriz[i];
+    if(monto == rev.monto ){
+      return i
+      }
+    }
+    return ""
+  };
+function borrar(id){
+  var tabla = localStorage.getItem("comprado");
+  tabla = JSON.parse(tabla);
+  var dev = eliminarSeleccion(id, tabla);
+  if(dev !== ''){
+  deletep(dev, tabla)
+};
+  tabla = JSON.stringify(tabla);
+  localStorage.setItem("comprado", tabla);
+  var tr = document.getElementById(id);
+  tr.parentNode.removeChild(tr);
+};
