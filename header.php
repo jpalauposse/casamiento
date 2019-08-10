47c97d530e6a5a2da5f4d437f7239e0e136d7d87
Javier Palau <javopa@yahoo.com.ar>
2019-08-10 17:20:13 -0300

    emparejando

--- header.php
+++ header.php
@@ -1,12 +1,17 @@
 <header class="cabeza">
   <nav class="navbar navbar-expand-md navbar-dark fixed-top barra">
-    <a class="navbar-brand" href="index.php">Pau y Javi</a>
+    <a class="navbar-brand" href="index.php">
+<img src="fotos/ICON.png" width="32" height="32" alt="Logo sitio">
+</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="navbar-brand" href="index.php">Pau y Javi</a>
+        </li>
         <li class="nav-item active">
           <a class="nav-link" href="pf.php">¿Como Comprar?<span class="sr-only">(current)</span></a>
         </li>
