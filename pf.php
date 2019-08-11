1c54e7a04669ed09f8bc9575ae8542e318886e1d
Javier Palau <javopa@yahoo.com.ar>
2019-08-11 14:10:41 -0300

    Se ajusta el Diseño de pf.html

--- pf.php
+++ pf.php
@@ -3,23 +3,56 @@
 <?php include 'head.php' ?>
 <body>
   <?php include 'header.php'?>
-  <main role="main">
-    <div class="container" id="preguntas">
-      <h1>¿Como comprar?</h1>
-      <p> Se puede elegir uno o mas programas.
-        <br>
-        Luego hacer click en el carrito <i class="fas fa-cart-arrow-down"></i> y le desplegara una lista de los programas elegidos.
-        <br>
-        En la lista podra avanzar al pago o quitar elementos que selecciono de más.
-        <br>
-        En el pago, se le dara la informacion de la cuenta donde realizar el deposito o transferencia.
-        <br>
-        Al finalizar el pago, no olvide enviar un mail con el comprobante de operacion.
-      </p>
-      <h2>Muchas gracias por su regalo!</h2>
-    </div>
-  </main>
-  <?php include 'footer.php'?>
+  <div class="container-fluid marketing" id="">
+    <div class="row">
+        <div class="col-xl-2 pf" id="izq">
+        </div>
+        <div class="col-xl-8 container text-center preguntas">
+            <h2 class="como_comprar" >¿Como comprar?</h2>
+            <h4 class="saludos" > Bienvenidos y gracias por regalar por pauyjavi.com.ar!</h4>
+            <div class="row pf">
+              <div class="col-1">
+                <img src="fotos/ICON.png" width="32" height="32" alt="Logo sitio" style="transform:rotate(90deg)">
+              </div>
+              <div class="col-11">
+                1) Elegir la opcion de regalo que les parezca mas divertida con el bonton . Pueden ser una o varias. </br> (clickeando en la opcion elegida se coloca automaticamente en el carrito)
+              </div>
+            </div>
+            <div class="row pf">
+              <div class="col-1">
+                <img src="fotos/ICON.png" width="32" height="32" alt="Logo sitio" style="transform:rotate(180deg)">
+              </div>
+              <div class="col-11">
+                2) Dirigirse al carrito <i class="fas fa-cart-arrow-down"></i> que aparece en el menu superior a la derecha. Alli se desplegara la lista de regalos elegidos y podra avanzar al pago o quitar opciones del listado
+              </div>
+            </div>
+            <div class="row pf">
+              <div class="col-1">
+                <img src="fotos/ICON.png" width="32" height="32" alt="Logo sitio" style="transform:rotate(270deg)">
+              </div>
+              <div class="col-11">
+                3) El pago podra realizarse por MercadoPago o por transferencia/Deposito. Alli podran completar su nombre y mail. (y nos encataria que tambien  agreguen sus saludos para los novios!)
+              </div>
+            </div>
+            <div class="row pf">
+              <div class="col-1">
+                <img src="fotos/ICON.png" width="32" height="32" alt="Logo sitio" style="transform:rotate(90deg)">
+              </div>
+              <div class="col-11">
+                 4) En caso de deposito o transferencia al finalizar el pago no olvide enviar un mail a noviosviajeros@pauyjavi.com.ar para poder agreadecerles directamente a su casilla!
+               </div>
+            </div>
+            <h2><img src="fotos/ICON.png" width="32" height="32" alt="Logo sitio"> Gracias!</h2>
+
+          <div class="row footer">
+              <?php include 'footer.php'?>
+          </div>
+          </div>
+          <div class="col-xl-2 pf" id="der">
+          </div>
+        </div>
+      </div>
+  </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
